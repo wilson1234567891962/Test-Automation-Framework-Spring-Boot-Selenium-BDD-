c3663260d7d4bd934634f03ae02427a9a7a55e84
LIZETH <lizethgio57@gmail.com>
2021-06-26 14:01:54 -0500

    Added the ConditionalOnMissingBean property when the code does not find the item

--- src/main/java/com/udemy/spring/springselenium/config/WebDriverConfig.java
+++ src/main/java/com/udemy/spring/springselenium/config/WebDriverConfig.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,7 @@ public class WebDriverConfig {
 	private int timeout;
 
     @Bean
+    @ConditionalOnMissingBean
     public WebDriver chromeDriver() {
         WebDriverManager.chromedriver().version("91.0.4472.101").setup();
         return new ChromeDriver();
